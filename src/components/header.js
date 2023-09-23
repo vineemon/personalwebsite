d92967de723dd8b7e124178a0fc5e2168b1e2832
vineemon <nadella.vineet@gatech.edu>
2023-09-23 18:55:22 -0400

    using joy UI instead of materialUI

--- src/components/header.js
+++ src/components/header.js
@@ -5,9 +5,11 @@ import linkedin from "../images/linkedin.png";
 import initials from "../images/initials.png";
 import Button from "@material-ui/core/Button";
 import IconButton from "@mui/material/IconButton";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import MoreVertIcon from "@mui/icons-material/MoreVert";
+import MoreVert from "@mui/icons-material/MoreVert";
+import MenuButton from '@mui/joy/MenuButton';
+import Menu from '@mui/joy/Menu';
+import MenuItem from '@mui/joy/MenuItem';
+import Dropdown from '@mui/joy/Dropdown';
 import { StyledButton } from "./styles";
 
 const ITEM_HEIGHT = 48;
@@ -54,44 +56,37 @@ const Header = ({ link }) => {
         }}
       ></StyledButton>
       {/* create a drop down for isTabletOrMobile */}
-      <div>
-        <IconButton
-          aria-label="more"
-          id="long-button"
-          aria-haspopup="true"
-          aria-controls={open ? "long-menu" : undefined}
-          aria-expanded={open ? "true" : undefined}
-          onClick={handleClick}
-        >
-          <MoreVertIcon style={{ color: "white" }} />
-        </IconButton>
-        <Menu
-          id="long-menu"
-          MenuListProps={{
-            "aria-labelledby": "long-button",
-          }}
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-          PaperProps={{
-            style: {
-              maxHeight: ITEM_HEIGHT * 4.5,
-              width: "15ch",
-              backgroundColor: "rgba(8, 40, 79, 1"
-            },
-          }}
-        >
-          <MenuItem href="/experience" component="a" style={{color:"white", fontFamily:"avenir" }}>
-            Experience
-          </MenuItem> 
-          <MenuItem href="/work" component="a" style={{color:"white", fontFamily:"avenir"  }}>
-            Work
-          </MenuItem>
-          <MenuItem href="/contact" component="a" style={{color:"white", fontFamily:"avenir"  }}>
-            Contact
-          </MenuItem>
-        </Menu>
-      </div>
+        <Dropdown>
+          <MenuButton
+            slots={{ root: IconButton }}
+            slotProps={{ root: { variant: "outlined", color: "neutral" } }}
+          >
+            <MoreVert style={{color: "white"}}/>
+          </MenuButton>
+          <Menu style={{backgroundColor: "rgba(8, 40, 79, 1)", variant: "outlined"}}>
+            <MenuItem
+              href="/experience"
+              component="a"
+              style={{ color: "white", fontFamily: "avenir" }}
+            >
+              Experience
+            </MenuItem>
+            <MenuItem
+              href="/work"
+              component="a"
+              style={{ color: "white", fontFamily: "avenir" }}
+            >
+              Work
+            </MenuItem>
+            <MenuItem
+              href="/contact"
+              component="a"
+              style={{ color: "white", fontFamily: "avenir" }}
+            >
+              Contact
+            </MenuItem>
+          </Menu>
+        </Dropdown>
     </header>
   );
 };
